e92e46c9f7423986e59f54732619a159aca108c6
JoseM333 <102648655+JoseM333@users.noreply.github.com>
2023-03-28 20:29:26 -0500

    Add files via upload
    LAB02

--- LAB02/Actividades/Principal.java
+++ LAB02/Actividades/Principal.java
@@ -1,18 +1,29 @@
-public class Principal{
+public class Principal {
     public static void main(String[] args) {
-        String cadena="la lluvia en Sevilla es una maravilla";
+        /*
+         * Bolsa<Golosina> bolsaCho = new Bolsa<Golosina>(0);
+         * Golosina c = new Golosina("milka");
+         * Golosina c1 = new Golosina("milka");
+         * Golosina c2 = new Golosina("ferrero");
+         * bolsaCho.add(c);
+         * bolsaCho.add(c1);
+         * bolsaCho.add(c2);
+         * for (Golosina Golosina : bolsaCho) {
+         * System.out.println(Golosina.getMarca());
+         * }
+         */
 
-        int contador=0;
-        for (int i=0;i<cadena.length();i++){
-            //Comprobamos si el caracter es una vocal
-            if(cadena.charAt(i)=='a' || 
-                cadena.charAt(i)=='e' || 
-                cadena.charAt(i)=='i' ||
-                cadena.charAt(i)=='o' ||
-                cadena.charAt(i)=='u'){
-                    contador++;
-                }
+        Bolsa<Golosina> bolsaGolo = new Bolsa<Golosina>(0);
+        Golosina c = new Golosina("milka", 34.0);
+        Golosina c1 = new Golosina("milka", 10.0);
+        Golosina c2 = new Golosina("ferrero", 20.0);
+        bolsaGolo.add(c);
+        bolsaGolo.add(c1);
+        bolsaGolo.add(c2);
+        for (Golosina Golosina : bolsaGolo) {
+            System.out.printf("Nombre: %s" + "\t" + "Peso: %.2f" + "\n", Golosina.getNombre(), Golosina.getPeso());// %s=String
+
         }
-        System.out.println("Hay "+contador+" vocales");
     }
+
 }
