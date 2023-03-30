2e9f30bf6e3b18a2697f16afe4d00ffb1db6b30e
Robert Dueñas <robert.duenas@ucsm.edu.pe>
2023-03-30 11:58:54 -0500

    Actualizacion de carpetas

--- LAB02/Actividades/CambioLetrasApp.java
+++
@@ -1,8 +0,0 @@
-public class CambioLetrasApp{
-    public static void main(String[] arg){
-        String cadena="La lluvia en Sevilla es una maravilla";
-
-        //Aviso, de esta forma no modifica el String original
-        System.out.print(cadena.replace('a', 'e'));
-    }
-}
--- LAB02/Actividades/Ejercicios_basicos_DDR_31.java
+++
@@ -1,6 +0,0 @@
-public class Ejercicios_basicos_DDR_31 {
-    public static void name(String[] agrs) {
-        String cadena="Hola mundo";
-        System.out.println("la cadena tiene "+cadena.length()+" caracteres");
-    }
-}
